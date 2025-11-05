739b459b79f6873d2fb09759b772718e9883171d
HexSecGuru <codejoaker13@gmail.com>
2025-11-05 20:38:13 +0200

    Initial Stracture1

---
+++ JAVASCRIPT/Lesson_2/If_else.java
@@ -0,0 +1,20 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+
+
+
+public class Main {
+  public static void main(String[] args) {
+   int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Give n: ");
+    n = sc.nextInt();
+    if ( n > 0){
+      System.out.println("n > 0");
+    }    
+    else
+      System.out.println("n <= 0");
+  }
+
+}
